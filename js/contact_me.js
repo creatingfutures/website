95043524e90d726a66d97e1c23094c79cfb4b1ef
Sarath <sarath.m76@gmail.com>
2023-08-02 12:47:15 +0530

    contact via mailto

--- js/contact_me.js
+++ js/contact_me.js
@@ -19,43 +19,47 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            //alert(name+" "+phone+" "+email+" "+message+" "+gender+" "+location);
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message,
-                    gender:gender,
-                    location:location
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+            let subject = "Volunteer Contact From CF Website: "+name;
+            let body = "You have received a new message from your CF website form for volunteering.\n\n"+`Here are the details:\n\nName: ${name}\n\nEmail: ${email}\n\nGender:${gender} \n\n Location:${location}. \n\n Phone: ${phone}\n\nMessage:\n${message}`;
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+            window.open('mailto:jimmy.cherian@creatingfutures.in?subject='+subject+'&body='+body);
+            //alert(name+" "+phone+" "+email+" "+message+" "+gender+" "+location);
+            // $.ajax({
+            //     url: "././mail/contact_me.php",
+            //     type: "POST",
+            //     data: {
+            //         name: name,
+            //         phone: phone,
+            //         email: email,
+            //         message: message,
+            //         gender:gender,
+            //         location:location
+            //     },
+            //     cache: false,
+            //     success: function() {
+            //         // Success message
+            //         $('#success').html("<div class='alert alert-success'>");
+            //         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            //             .append("</button>");
+            //         $('#success > .alert-success')
+            //             .append("<strong>Your message has been sent. </strong>");
+            //         $('#success > .alert-success')
+            //             .append('</div>');
+
+            //         //clear all fields
+            //         $('#contactForm').trigger("reset");
+            //     },
+            //     error: function() {
+            //         // Fail message
+            //         $('#success').html("<div class='alert alert-danger'>");
+            //         $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            //             .append("</button>");
+            //         $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+            //         $('#success > .alert-danger').append('</div>');
+            //         //clear all fields
+            //         $('#contactForm').trigger("reset");
+            //     },
+            // })
         },
         filter: function() {
             return $(this).is(":visible");
